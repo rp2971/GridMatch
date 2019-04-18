31b35ac6dbec5158585e8e68550143f923fb68e1
Raveena <rp2971@nyu.edu>
2019-04-18 10:24:35 -0400

    Grid layout + randomization colors

---
+++ SwapMatch/Assets/Script/Grid.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour
+{
+
+    private GameObject[,] tiles;
+    public GameObject[] Player; 
+
+    //private GameObject[] tileArray = new GameObject[4];
+
+    public Vector3 Origin;
+    public int width;
+    public int height; 
+
+    public GameObject tileinstance; // giving a reference to what we are instantiating (the gameobject) 
+    public GameObject Playerinstance;
+
+    public List<Color> colorList = new List<Color>();
+
+    //public Color red = Color.red;
+    //public Color black = Color.black;
+    //public Color blue = Color.blue;
+    //public Color yellow = Color.yellow;
+    //public Color magenta = Color.magenta;
+    //public Color cyan = Color.cyan;
+
+    private void Awake() // awake to instantiate numbers
+    {
+        tiles = new GameObject[width, height]; //2D array
+    }
+
+    private void Start()  
+    {
+        //GameObject Player = Instantiate(Playerinstance, new Vector3(0, 0, 0), Quaternion.identity); // creating a new tile + filling it
+        //GameObject tile2 = Instantiate(tileinstance, new Vector3(1, 0, 0), Quaternion.identity); 
+
+        int counterX = 0;
+        //int counterY = 0;
+
+        for (int j = 0; j < height; j++) // j = cols
+        {
+        
+            for (int i = 0; i < width; i++) // i = rows
+            {
+                tiles[i,j] = Instantiate(tileinstance, Origin + new Vector3(i,j,0), Quaternion.identity);
+                counterX++;
+                tiles[i, j].GetComponent<SpriteRenderer>().color = colorList[Random.Range(0, 6)];
+
+            }
+
+            //counterX = 0;
+            //counterY++;
+
+        }
+
+        //tiles[0, 0].GetComponent<SpriteRenderer>().color = colorList[Random.Range(0, 5)];
+        //tiles[1, 0].GetComponent<SpriteRenderer>().color = colorList[Random.Range(0, 6)];
+        //tiles[2, 0].GetComponent<SpriteRenderer>().color = colorList[Random.Range(0, 6)];
+        //tiles[3, 0].GetComponent<SpriteRenderer>().color = colorList[Random.Range(0, 6)];
+    }
+}
--- SwapMatch/Assets/Script/GridManager.cs
+++ SwapMatch/Assets/Script/GridManager.cs
@@ -1,1 +1,38 @@
-﻿using System.Collections.Generic; using UnityEngine;  public class GridManager : MonoBehaviour {     public const int COLS = 6;     public const int ROWS = 3;     int[,] _gems = new int[COLS, ROWS];      void Start()     {          _gems[0, 0] = Random.Range(0, 9);         _gems[0, 1] = Random.Range(0, 9);         _gems[0, 2] = Random.Range(0, 9);         _gems[1, 0] = Random.Range(0, 9);         // _gems[2,1] = 5;          for (int x = 0; x < COLS; x++)         {             for (int y = 0; y < ROWS; y++)             {                 int color = Random.Range(0, 9);                 _gems[x, y] = color;             } // internal loop will go until it is finished          }          for(int x = 0; x < COLS; x++)        {             for (int y = 0; y < ROWS; y++)             {                 Debug.Log("Gem" + x + "," + y + ":" + _gems[x,y]); // using the plus sign to stick the strings together - to log the message             }        }       }      void Update()     {      } }  
+﻿using System.Collections.Generic; using UnityEngine;  public class GridManager : MonoBehaviour {     //public const int COLS = 6;     //public const int ROWS = 3;     //int[,] _gems = new int[COLS, ROWS];      //void Start()     //{      //    _gems[0, 0] = Random.Range(0, 9);     //    _gems[0, 1] = Random.Range(0, 9);     //    _gems[0, 2] = Random.Range(0, 9);     //    _gems[1, 0] = Random.Range(0, 9);     //    // _gems[2,1] = 5;      //    for (int x = 0; x < COLS; x++)     //    {     //        for (int y = 0; y < ROWS; y++)     //        {     //            int color = Random.Range(0, 9);     //            _gems[x, y] = color;     //        } // internal loop will go until it is finished      //    }       //   for(int x = 0; x < COLS; x++)     //   {     //        for (int y = 0; y < ROWS; y++)     //        {     //            Debug.Log("Gem" + x + "," + y + ":" + _gems[x,y]); // using the plus sign to stick the strings together - to log the message     //        }     //   }       //}      //void Update()     //{      //}      //int CheckColumn(GameObject currentCell, int col, int row)
+    //{
+    //    int matches = 1;
+    //    List<GameObject> matches = new List<GameObject>();     //    for (int tempRow = row + 1; tempRow < HEIGHT; ++row)//going to start the row after
+    //    {     //        GameObject tempCell = tokens[col, tempRow];     //        if (tempCell.color == currentCell.color)
+    //        {
+    //            matches.Add(tempCell);
+    //        }     //        else
+    //        {
+    //            break;
+    //        }      //        if (matches.Count > 3)
+    //        {
+    //            //do matching stuff 
+    //        }     //    }
+    //}      //public void CheckForMatches()
+    //{
+    //    GameObject currentCell;     //    for(int col = 0; col < WIDTH; ++col)
+    //    {
+    //        for (int row = 0; col < HEIGHT; ++row )
+    //        {
+    //                currentCell = tokens[col, row];     //                tempList = CheckColumn(currentCell, col, row);      //                foreach (GameObject go in tempList)
+    //                {
+    //                    gemsToDestroy(); // not sure if this is right 
+    //                }     //        }
+    //    }
+    //}      //public void MakeGemsFall()
+    //{
+    //    for (int col = HEIGHT-1; col >=0; col--)
+    //    {
+    //        for(int row = WIDTH -1; row >= 0; row--)
+    //        {
+    //            GameObject currentToken = tokens[col, row];     //            int distance = 1;     //            GameObject destination = tokens[col, row + distance];     //            if(tokens[col, row+1] == null)
+    //            {
+    //                //it needs to fall
+    //            }
+    //        }
+    //    }
+    //} }   
